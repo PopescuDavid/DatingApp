cbc310fbb487105d85ebf0156bc2fa5130ab3070
David <davidpopescu2002@gmail.com>
2024-10-29 14:56:13 +0200

    End of section 9 - Finished building the UI

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -4,7 +4,6 @@ using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Query;
 
 namespace API.Data;
 
